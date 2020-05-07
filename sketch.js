ccd61299374617352c8bdbdfe4c6707398589733
winniewyc2020 <wchen15@saic.edu>
2020-05-07 11:14:16 +0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -16,10 +16,10 @@ function setup() {
   capture = createCapture(VIDEO);
   capture.size(400, 300);// size of webcam view
   capture.hide();
-  video1.addCue(31, webcamA);//call the funtion changeVideo below after 31 seconds into playing video1 
-  video1.addCue(38, webcamOff);//call the funtion changeVideo below after 5 seconds into playing video2
-  video1.addCue(47, webcamB);//call the funtion changeVideo below after 5 seconds into playing video2
-  video1.addCue(120, webcamOff);//call the funtion changeVideo below after 5 seconds into playing video2
+  video1.addCue(30.8, webcamA);//call the funtion changeVideo below after 31 seconds into playing video1 
+  video1.addCue(37.3, webcamOff);//call the funtion changeVideo below after 5 seconds into playing video2
+  video1.addCue(45.7, webcamB);//call the funtion changeVideo below after 5 seconds into playing video2
+  video1.addCue(120.3, webcamOff);//call the funtion changeVideo below after 5 seconds into playing video2
   vid = video1; //loading the first video we want to play at the beginning
 
 }
@@ -40,23 +40,22 @@ wy=0;
 
 function webcamA() {
   //large mode
-  wx = width/3;
+  wx = width/3.5;
   wy = height/4;
-  wwidth = width/3+100;
-  wheight = height/3+50;
+  wwidth = width/3+140;
+  wheight = height/3+75;
 }
 
 function webcamB(){
   //small mode
-  wx = width/2 +4;
-  wy = 10;
-  wwidth = width/3;
-  wheight = height/3;
+  wx = width/2 +3;
+  wy = 14;
+  wwidth = width/3-16;
+  wheight = height/3-3;
 }
 
 function draw() {
   image(vid, 0, 0, width, height);//draw the video to the canvas
   image(capture, wx, wy, wwidth, wheight);
 } 
- 
 
